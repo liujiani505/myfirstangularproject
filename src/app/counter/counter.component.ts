82ae91e1a022ddd3ae68341270aca16f62c69bf1
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-19 22:10:57 -0400

    generated data service

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-counter',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  constructor() { }
+  counter = 0;
+  cheese = "gouda";
+  data:DataService;
+
+  constructor(data:DataService) {
+    this.data = data
+  }
+
+  addOne(){
+    this.counter +=1
+  }
 
   ngOnInit(): void {
   }
